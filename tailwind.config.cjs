841a7c0b08c03aa324debbce99f2d947368b6963
Rootzreggae <aosparesdetres@gmail.com>
2022-01-10 08:30:25 +0000

    fix broken padding

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -27,21 +27,4 @@ const config = {
 ],
 };
 
-module.exports = {
-
-  // add daisyUI plugin
-  plugins: [
-    require('daisyui'),
-  ],
-
-  // config (optional)
-  daisyui: {
-    styled: true,
-    themes: true,
-    base: true,
-    utils: true,
-    logs: true,
-    rtl: false,
-  },
-
-}
+module.exports = config;
