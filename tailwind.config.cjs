5c1f72c51fa31023d10ef3c67aa2ac8ea24d70e7
Rootzreggae <aosparesdetres@gmail.com>
2022-01-10 08:25:14 +0000

    dark mode default fix

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -27,4 +27,21 @@ const config = {
 ],
 };
 
-module.exports = config;
+module.exports = {
+
+  // add daisyUI plugin
+  plugins: [
+    require('daisyui'),
+  ],
+
+  // config (optional)
+  daisyui: {
+    styled: true,
+    themes: true,
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+  },
+
+}
