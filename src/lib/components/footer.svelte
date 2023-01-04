c38c98fd5a41bd421e6b7ad2f28c9af004f37cbd
Rootzreggae <aosparesdetres@gmail.com>
2023-01-04 09:13:14 +0000

    Updated year

--- src/lib/components/footer.svelte
+++ src/lib/components/footer.svelte
@@ -5,7 +5,7 @@
 <footer class="items-center p-4 footer bg-neutral text-neutral-content">
   <div class="items-center grid-flow-col">
    
-    <p>© 2022 Nilson Gaspar</p>
+    <p>© 2023 Nilson Gaspar</p>
   </div>
   <div class="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
     <a href="https://www.linkedin.com/in/nilsongaspar/">
